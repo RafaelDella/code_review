8a12d272eca42ec742a1b76e10854d98631ee3e8
Rafael Della Giustina <62893087+RafaelDella@users.noreply.github.com>
2025-09-16 19:03:36 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # code_review
+
+Equipe: Eduardo Golin Scholz e Rafael Della Giustina
---
+++ solucoes.py
@@ -0,0 +1,25 @@
+# Arquivo: solucoes.py
+
+def sao_anagramas(string1, string2):
+    """
+    Verifica se duas strings são anagramas uma da outra, ignorando
+    espaços e diferenças entre maiúsculas e minúsculas.
+    """
+    # TODO: Implementar a lógica
+    pass
+
+def cifra_de_cesar(texto, deslocamento):
+    """
+    Aplica a Cifra de César a uma string, preservando a capitalização e
+    caracteres que não sejam letras.
+    """
+    # TODO: Implementar a lógica
+    pass
+
+def encontrar_maior_palavra(frase):
+    """
+    Encontra a maior palavra em uma frase, ignorando a pontuação.
+    Em caso de empate no comprimento, retorna a primeira palavra encontrada.
+    """
+    # TODO: Implementar a lógica
+    pass
